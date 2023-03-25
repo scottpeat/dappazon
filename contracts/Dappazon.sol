99ace19bbe29e079b37852336714e79e4f090faf
Scott Peat <scottpeatweb@gmail.com>
2023-03-25 15:45:58 +0000

    Starting work on smart contracts and testing with chai

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => item) public items;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // List products
+    function list(uint256 _id,
+    string memory _name,
+    string memory _category,
+    string memory _image,
+    uint256 _cost,
+    uint256 _rating,
+    uint256 _stock
+    ) public {
+        // Create Item Struct
+        Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stock);
+        // Save Item Struct to chain
+        items[_id] = item;
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,30 @@
-const { expect } = require("chai")
+const { expect } = require('chai');
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+};
 
-describe("Dappazon", () => {
+describe('Dappazon', () => {
+  let deployer, buyer;
 
-})
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, buyer] = await ethers.getSigners();
+    console.log(deployer.address, buyer.address);
+    // Deploy smart contract
+    const Dappazon = await ethers.getContractFactory('Dappazon');
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe('Deployment', () => {
+    it('Sets the owner', async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    });
+  });
+
+  describe('Listing', () => {
+    it('Returns item attributes', async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    });
+  });
+});
