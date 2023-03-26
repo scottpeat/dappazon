40d76feee28c103f58484acb1f4250b4910ef571
Scott Peat <scottpeatweb@gmail.com>
2023-03-26 14:18:25 +0100

    Started work on frontend and completed Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,28 @@
 import { ethers } from 'ethers';
 
 const Navigation = ({ account, setAccount }) => {
-
-    return (
-        <nav>
-
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <div className="nav__brand">
+        <h1>Dappazon</h1>
+      </div>
+      <input type="text" className="nav__search" />
+      <button type="button" className="nav__connect">
+        {account.slice(0, 6) + '...' + account.slice(38, 42)}
+      </button>
+      <ul className="nav__links">
+        <li>
+          <a href="#Clothing & Jewellery">Clothing & Jewellery</a>
+        </li>
+        <li>
+          <a href="#Electronics & Gadgets">Electronics & Gadgets</a>
+        </li>
+        <li>
+          <a href="#Toys & Gaming">Toys & Gaming</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
